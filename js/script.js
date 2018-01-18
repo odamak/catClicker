b3ca1fed01e62a840e0952f93ef7eec57c778c7d
unknown <omardamak1@gmail.com>
2018-01-18 18:52:08 +0100

    MVC works properly

--- js/script.js
+++ js/script.js
@@ -40,49 +40,48 @@ $(function(){
             }
         },
         bindButtonToCat: function(idNumber) {
-	        $("#button"+idNumber).click(function(){
-		        viewCat.hideAllCats();
-		        $("#cat"+idNumber).show();
-	        })
+            $("#button"+idNumber).click(function(){
+                viewCat.renderCat(idNumber);
+            })
+        },
+        bindButtonsToCat: function(){
+            for(let i=0; i< NUMBER_CATS; i++)
+                this.bindButtonToCat(i);
         },
         init: function() {
             this.displayButtons();
-            for (var i=0; i< NUMBER_CATS; i++){
-	        this.bindButtonToCat(i);
-            }
+            this.bindButtonsToCat();
         }
     }
 
     var viewCat = {
         cats : $(".cat"),
-        hideAllCats: function() {
-            for (var i=0; i<NUMBER_CATS; i++){
-		    $(this.cats[i]).hide();
-	        }
+        renderCat: function(idNumber) {
+            $( ".cat" ).remove();
+            let str = `<div class="cat" id="cat`+ idNumber + `" >
+            <span class="counter">${octopus.getCounter(idNumber)}</span><span> clicks</span>`;
+            $("div").last().after(str);
+            let catIdIdentifier= '#cat'+idNumber;
+            let identifier = catIdIdentifier+" "+"span:nth-child(2)";
+            $(identifier).after("<br>");
+            $(identifier).next().after(`<img class="clicker" src="pictures/cat_picture${idNumber}.jpeg">
+            </div>`);
+            let newCounter = this.clickOnCat(idNumber);          
+
         },
-        bindCounterToCat: function (idNumber){
-            let catId = 'cat' + idNumber;
-            $('#' + catId + ' span:nth-child(2)').val(octopus.getCounter(idNumber));
-        },
+
+        clickOnCat: function(idNumber){
+            $("img").click(function(){
+                // alert("Handler for img called "+idNumber);
+                octopus.updateCounter(idNumber);
+                let newCounter = octopus.getCounter(idNumber);
+                $(".counter").text(newCounter);
+                return(newCounter);
+            });
+
+        },        
         init: function() {
-                this.renderCats();
-                for (var i=0; i<NUMBER_CATS; i++){
-	            this.bindCounterToCat(i);
-                }
-                this.hideAllCats();
-                $("#cat0").show();       
-        },
-        renderCats: function(){
-            for (let i=0; i<NUMBER_CATS; i++ ){
-                let str = `<div class="cat" id="cat`+ i + `" >
-                <span class="counter">${octopus.getCounter(i)}</span><span> clicks</span>`;
-                $("div").last().after(str);
-                let catIdIdentifier= '#cat'+i;
-                let identifier = catIdIdentifier+" "+"span:nth-child(2)";
-                $(identifier).after("<br>");
-                $(identifier).next().after(`<img class="clicker" src="pictures/cat_picture${i}.jpeg">
-                </div>`);
-            }
+            this.renderCat(0);       
         }
     };
 
