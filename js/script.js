829f81c59bff4132000936f8ca7769848846aa33
odamak <omardamak1@gmail.com>
2018-01-19 18:09:22 +0100

    Refactor code, display cats, button doesn't work

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-$(function(){
+$(function () {
     const NUMBER_CATS = 5;
 
     var model = {
-        cats : [],
+        cats: [],
         currentCat: null,
-        init: function() {
-                for (let i = 0; i<NUMBER_CATS; i++){
-                    let elem = new Object()
-                    elem.index = i;
-                    elem.name = 'cat'+i;
-                    elem.clickCount = 0;
-                    elem.photo = 'pictures/cat_picture'+i+'.jpg';
-                    this.cats.push(elem);
-                }
-                currentCat = this.cats[0];
-                return this.cats;
+        init: function () {
+            for (let i = 0; i < NUMBER_CATS; i++) {
+                let elem = new Object()
+                elem.index = i;
+                elem.name = 'cat' + i;
+                elem.clickCount = 0;
+                elem.photo = 'pictures/cat_picture' + i + '.jpeg';
+                this.cats.push(elem);
+            }
+            currentCat = this.cats[0];
+            return this.cats;
         }
     };
 
     var octopus = {
-        init: function() {
+        init: function () {
             model.init();
             viewButton.init();
             viewCat.init();
         },
-        getCurrentCat: function(){
+        getCurrentCat: function () {
             return model.currentCat;
         },
-        setCurrentCat: function(catIndex) {
+        setCurrentCat: function (catIndex) {
             //save previous changes made on currentCat
             model.cats[model.currentCat.index] = model.currentCat
             //get the new selected cat and update value of currentCat
@@ -38,66 +38,45 @@ $(function(){
             //return the new value of currentCat
             return selectedCat;
         },
-        updateCounterCurrentCat: function() {
+        updateCounterCurrentCat: function () {
             model.currentCat.clickCount += 1
             return (model.currentCat.clickCount);
         },
-        getCats: function(){
+        getCats: function () {
             return (model.cats)
         }
     };
 
     var viewButton = {
-        init: function() {
+        init: function () {
             let cats = octopus.getCats();
-            for (let i=0; i< NUMBER_CATS; i++){
-                let $button = `<button id='button${i}'>${cats[i].name}</button>`;
-                $button.appendTo($("#catButtons"));
+            for (let i = 0; i < NUMBER_CATS; i++) {
+                let buttonHtml = `<button id='button${i}'>${cats[i].name}</button>`;
+                $(buttonHtml).appendTo($("#catButtons"));
+                $("#button" + i).click(function () {
+                    octopus.setCurrentCat(i);
+                })
             }
-        },
-        render: function(idNumber) {
-            $("#button"+idNumber).click(function(){
-                octopus.setCurrentCat(idNumber);
-            })
-        },
-        init: function() {
-            this.displayButtons();
-            this.bindButtonsToCat();
         }
     }
 
     var viewCat = {
-        cats : $(".cat"),
-        renderCat: function(idNumber) {
-            $( ".cat" ).remove();
-            let str = `<div class="cat" id="cat`+ idNumber + `" >
-            <span class="counter">${octopus.getCounter(idNumber)}</span><span> clicks</span>`;
-            $("div").last().after(str);
-            let catIdIdentifier= '#cat'+idNumber;
-            let identifier = catIdIdentifier+" "+"span:nth-child(2)";
-            $(identifier).after("<br>");
-            $(identifier).next().after(`<img class="clicker" src="pictures/cat_picture${idNumber}.jpeg">
-            </div>`);
-            let newCounter = this.clickOnCat(idNumber);          
-
+        render: function () {
+            let currentCat = octopus.getCurrentCat();
+            $("#cat-name").text(currentCat.name);
+            $("#cat-count").text(currentCat.clickCount);
+            $("#cat-img").attr("src",currentCat.photo);
         },
 
-        clickOnCat: function(idNumber){
-            $("img").click(function(){
-                // alert("Handler for img called "+idNumber);
-                octopus.updateCounter(idNumber);
-                let newCounter = octopus.getCounter(idNumber);
-                $(".counter").text(newCounter);
-                return(newCounter);
-            });
-
-        },        
-        init: function() {
-            this.renderCat(0);       
+        init: function () {
+            let cats = octopus.getCats();
+            $("#cat-name").text(cats[0].name);
+            $("#cat-count").text(cats[0].clickCount);
+            $("#cat-img").attr("src",cats[0].photo);
         }
     };
 
     octopus.init();
-    
+
 });
 
